d591cab240485c8019a45cedbace5d385106b2e1
Oliwia Rogowska <90906831+rogowska@users.noreply.github.com>
2021-12-25 20:29:52 +0100

    implementacja macierzy w C

---
+++ projektzal/macierz.c
@@ -0,0 +1,12 @@
+int main(){
+    int kolumny, wiersze, i, j;
+    double macierz;
+    macierz=
+
+    printf("%s\n", "Podaj liczbę wierszy macierzy:");
+    scanf("%i\n", &wiersze);
+    printf("%s\n", "Podaj liczbę kolumn macierzy:");
+    scanf("%i\n", &kolumny);
+
+    return 0;
+}
