b9594af5bd0eee227b0fef71b046095847295a5b
rogowska <rogowska.oliwia@gmail.com>
2021-12-26 17:35:42 +0100

    main

---
+++ projektzal/main_projektzal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int wiersze, i, j, kolumny;
+
+    printf("%s\n", "Podaj liczbę wierszy macierzy:");
+    scanf("%i", &wiersze);
+    printf("%s\n", "Podaj liczbę kolumn macierzy:");
+    scanf("%i", &kolumny);
+
+    int **macierz = calloc(kolumny, sizeof(int *));
+    for (i = 0; i < kolumny; i++)
+    {
+        macierz[i] = calloc(wiersze, sizeof(int));
+    }
+
+    for (i = 0; i < wiersze; i++)
+    {
+        for (j = 0; j < kolumny; j++)
+        {
+            printf("%i", macierz[j][i]=rand()%2);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
