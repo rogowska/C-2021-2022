de573f462a23e77403c405196a141549f726ca78
brogowski <brogowski@o2.pl>
2022-01-07 14:01:02 +0100

    propozycje zmian i modyfikacji

--- projektzal/zabezpieczenia.c
+++ projektzal/zabezpieczenia.c
@@ -1,4 +1,45 @@
 /*Oliwia Rogowska*/
+/*@ZR 
+ - propozycja: nazwa pliku wynikowego life
+ - zmiana nazwy funkcji zabezpieczenia na inputValidator
+ - przekazanie przez wskaźnik wartośći columns, rows, steps w funkcji inputValidator
+ - nazewnictwo w języku angielskim
+ - komunikacja z uzytkownikiem j.w.
+ - zrezygnowanie z trybu interaktywnego na rzecz argumentów wywowałania z commandline
+  getopt https://azrael.digipen.edu/~mmead/www/Courses/CS180/getopt.html
+ - max value dla columns i rows 10000 - czas obliczeniowy rośnie do 13sec, wymagana pamięc ~800MB
+ - propozycja na help:
+
+life --help
+
+usage:  life --columns --rows --steps [--delay]
+    or
+        life --tryout
+
+Symulator game of 'Life' based on 23/3 Johna Conwaya rule with pseudo-random initial conditions.
+
+Mandatory arguments to long options are mandatory for short options too.
+
+       -c, --columns=n
+              column size of board as positive value in range <1-10000>
+
+       -d, --delay=n
+              delay in seconds between printing current board (default 1)
+
+       -h, --help
+              display this help and exit
+
+       -r, --rows=n
+              rows size of board as positive value in range <1-10000>
+
+       -s, --steps=n
+              number of simulation steps, positive natural number
+
+       -t, --tryout
+              run demo, predifine usaged arguments: -c 40 -r 20 -s 100
+
+*/
+
 #include <stdio.h>
 
 int zabezpieczenia(int wiersze, int kolumny, int kroki){
