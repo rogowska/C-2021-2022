1c6cfdca4741159b9f4ad5efd7f1fa5a2d81a5de
Gabriela Rogowska <37714253+GabRog@users.noreply.github.com>
2022-01-07 22:30:53 +0100

    Zmiany nazw w 'logika.c'

--- projektzal/logika.c
+++ projektzal/logika.c
@@ -1,7 +1,9 @@
 /*Oliwia Rogowska*/
 #include "logika.h"
 
-int logika(int sasiedzi, int i, int j, int **macierz_current)
+int logika(int sasiedzi, int i, int j, int **macierz_current) // funkcje powinny być czasownikami, tu proponuję: generate_next_state
+    // jako argumenty: neighbours, row_index, column_index, current_matrix - w związku z tym zmiana zmiennych w całym kodzie
+    
 /*implementacja logiki wg Conwaya - 
 decydowanie o stanie komorki na podstawie liczby sasiadow i jej stanu poprzedniego*/
 {
