cd996df9d4466b082302c773d370b50dd015682f
Gabriela Rogowska <37714253+GabRog@users.noreply.github.com>
2022-01-07 22:25:37 +0100

    Update drukowanie_macierzy.c

--- projektzal/drukowanie_macierzy.c
+++ projektzal/drukowanie_macierzy.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include "drukowanie_macierzy.h"
 
-int drukowanie_macierzy(int wiersze, int kolumny, int **macierz)
+int drukowanie_macierzy(int wiersze, int kolumny, int **macierz) // drukowanie_macierzy --> print_matrix
 /*funkcja drukujaca macierz na ekranie terminalu*/
 {
-    int i, j;
-    for (i = 0; i < wiersze; i++)
+    int i, j; // i --> row_index, j --> column_index
+    for (i = 0; i < wiersze; i++) // wiersze --> rows
     {
-        for (j = 0; j < kolumny; j++)
+        for (j = 0; j < kolumny; j++) // kolumny --> columns
         {
-            if (macierz[j][i]==1)
+            if (macierz[j][i]==1) // macierz --> matrix
                 {
                     printf("*");
                 }
